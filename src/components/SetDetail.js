307b032198ff266fc731a18d2352f62ca1744fc7
Jess Poemape <jesspoemape@gmail.com>
2017-07-18 14:51:37 -0600

    set detail header and circle icons are styled

---
+++ src/components/SetDetail.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from './Header';
+
+const SetDetail = () => {
+    return (
+        <div>
+            <Header/>
+            <div className='set-user-header'>
+                <h4 className='dark-label'>14 terms</h4>
+                <p className='set-user-p'>srtapoemape</p>
+            </div>
+            <div className='set-title-header'>
+                <h1>Spanish Colors (Los Colores)</h1>
+            </div>
+            <div className='set-description-header'>
+                <h4>Hello, this is the description</h4>
+            </div>
+            <div className='circle-icons-container'>
+                <div className='circle-icon'>
+                    <svg id='copy' className='set-circle-svg' xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" version="1.1" x="0px" y="0px"><title>7.4</title><desc>Created with Sketch.</desc><g stroke="none" stroke-width="1" fillRule="evenodd"><g fillRule="nonzero"><g><path d="M20,36 L20,40 L24.9991283,40 L2.00174332,40 C0.89621101,40 0,39.1084602 0,38.0027084 L0,1.99729162 C0,0.894217919 0.889261723,0 2.00174332,0 L29.9982567,0 C31.103789,0 32,0.891539827 32,1.99729162 L32,26.0013542 L32,14 L28,14 L28,4 L4,4 L4,36 L10,36 L10,40 L16,40 L16,36 L20,36 Z M28,4 L28,10 L32,10 L32,4 L28,4 Z M16,12.0028592 C16,10.8967106 16.8892617,10 18.0017433,10 L45.9982567,10 C47.103789,10 48,10.9018508 48,12.0028592 L48,45.9971408 C48,47.1032894 47.1107383,48 45.9982567,48 L18.0017433,48 C16.896211,48 16,47.0981492 16,45.9971408 L16,12.0028592 Z M20,44 L44,44 L44,14 L20,14 L20,44 Z M8,38 C8,36.8954305 8.89826062,36 9.99791312,36 L12.0020869,36 C13.1055038,36 14,36.8877296 14,38 C14,39.1045695 13.1017394,40 12.0020869,40 L9.99791312,40 C8.89449617,40 8,39.1122704 8,38 Z M30,2 C31.1045695,2 32,2.89826062 32,3.99791312 L32,6.00208688 C32,7.10550383 31.1122704,8 30,8 C28.8954305,8 28,7.10173938 28,6.00208688 L28,3.99791312 C28,2.89449617 28.8877296,2 30,2 Z"/></g></g></g></svg>
+                </div>
+                <div className='circle-icon'>
+                    <svg id='share' className='set-circle-svg' xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" space="preserve"><g><path d="M60,40c-23.5,0-44,16-49.7,38.8L10,80c12.8-12.8,30.2-20,48.3-20H60v20l30-30L60,20V40z"/></g></svg>
+                </div>
+                <div className='circle-icon'>
+                    <svg id='plus' className='set-circle-svg' xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" ><rect x="5" y="42.5" width="90" height="15"/><rect x="42.5" y="5" width="15" height="90"/></svg>
+                </div>
+                <div className='circle-icon'>
+                    <svg id='info' className='set-circle-svg' xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 32 32"><g display="none"><g display="inline"><path stroke="#666666" stroke-miterlimit="10" d="M13.813,4.877V0.5h4.554v4.377H13.813z M13.813,31.5V9.043h4.554    V31.5H13.813z"/></g></g><g display="none"><g display="inline"><g><path d="M18.866,32h-5.554V8.543h5.554V32z M14.312,31h3.554V9.543h-3.554V31z M18.866,5.377h-5.554V0h5.554     V5.377z M14.312,4.377h3.554V1h-3.554V4.377z"/></g></g></g><g display="none"><g display="inline"><g><path d="M18.866,32h-5.554V8.543h5.554V32z M14.312,31h3.554V9.543h-3.554V31z M18.866,5.377h-5.554V0h5.554     V5.377z M14.312,4.377h3.554V1h-3.554V4.377z"/></g></g></g><g><rect x="13.634" width="4.733" height="4.518"/><rect x="13.634" y="8.818" width="4.733" height="23.182"/></g></svg>
+                </div>
+                <div className='circle-icon'>
+                    <svg id="more" className='set-circle-svg' viewBox="0 0 22 6" width="100%" height="100%"><path d="M3.324.266a2.48 2.48 0 0 0-1.816.761 2.48 2.48 0 0 0-.762 1.817c0 .703.254 1.308.762 1.816a2.48 2.48 0 0 0 1.816.762 2.48 2.48 0 0 0 1.817-.762 2.48 2.48 0 0 0 .761-1.816 2.48 2.48 0 0 0-.761-1.817A2.48 2.48 0 0 0 3.324.266zm15.352 0c-.703 0-1.299.254-1.787.761a2.53 2.53 0 0 0-.733 1.817c0 .703.244 1.308.733 1.816a2.385 2.385 0 0 0 1.787.762 2.48 2.48 0 0 0 1.816-.762 2.48 2.48 0 0 0 .762-1.816 2.48 2.48 0 0 0-.762-1.817 2.48 2.48 0 0 0-1.816-.761zM11 .266a2.48 2.48 0 0 0-1.816.761 2.48 2.48 0 0 0-.762 1.817c0 .703.254 1.308.762 1.816A2.48 2.48 0 0 0 11 5.422a2.48 2.48 0 0 0 1.816-.762 2.48 2.48 0 0 0 .762-1.816 2.48 2.48 0 0 0-.762-1.817A2.48 2.48 0 0 0 11 .266z" fillRule="evenodd"></path></svg>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SetDetail;
--- src/router.js
+++ src/router.js
@@ -4,11 +4,13 @@ import {Switch, Route} from 'react-router-dom';
 import CreateSet from './components/CreateSet';
 import LandingPage from './components/LandingPage';
 import LatestActivity from './components/LatestActivity';
+import SetDetail from './components/SetDetail';
 
 export default (
     <Switch>
         <Route exact path='/' component={LandingPage}/>
         <Route path='/create-set' component={CreateSet}/>
         <Route path='/activity' component={LatestActivity}/>
+        <Route path='/set-detail' component={SetDetail}/>
     </Switch>
 );
