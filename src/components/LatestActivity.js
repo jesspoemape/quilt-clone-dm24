c0ef5e386ab68f4906ea96cdef0fcd0f71a381ef
Jess Poemape <jesspoemape@gmail.com>
2017-07-18 13:50:00 -0600

    latest activity initial styling

--- src/components/LatestActivity.js
+++ src/components/LatestActivity.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class LatestActivity extends Component {
     render() {
         return (
             <div>
-                latest activity
+                <Header/>
+                <div className='activity-main-container'>
+                    <h1 className='dark-header'>Latest Activity</h1>
+                    <input type="text" placeholder='Search your sets by title' className='create-input'/>
+                    <h4 className='create-title-label'>FILTER SETS</h4>
+                </div>
+                <div className='activity-sets-container'>
+                    <h4 className='dark-label'>LAST WEEK</h4>
+                </div>
             </div>
         );
     }
