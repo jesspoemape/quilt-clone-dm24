4e866cd74893d960717b4d91b495cc4db16a33ec
Jess Poemape <jesspoemape@gmail.com>
2017-07-18 13:53:06 -0600

    fixed typo in header svg. latest activity search bar captures change

--- src/components/LatestActivity.js
+++ src/components/LatestActivity.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class LatestActivity extends Component {
+    constructor() {
+        super();
+
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(e) {
+        console.log(e);
+    }
+
     render() {
         return (
             <div>
                 <Header/>
                 <div className='activity-main-container'>
                     <h1 className='dark-header'>Latest Activity</h1>
-                    <input type="text" placeholder='Search your sets by title' className='create-input'/>
+                    <input 
+                        type="text" 
+                        placeholder='Search your sets by title' 
+                        className='create-input'
+                        onChange={ e => this.handleSearch(e.target.value)}
+                    />
                     <h4 className='create-title-label'>FILTER SETS</h4>
                 </div>
                 <div className='activity-sets-container'>
