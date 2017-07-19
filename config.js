bfdc3d5792340d2d41b83774af5e53a9976a9f5e
Jess Poemape <jesspoemape@gmail.com>
2017-07-19 15:09:38 -0600

    auth set up and endpoints written, not tested. landing login page has initial styling

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "connectionString": "postgres://drwdmhrdwpwkvq:e0e877b477553a7d199b57cefd375252fa78697862ad6df4970351a2cbb8741a@ec2-23-21-96-159.compute-1.amazonaws.com:5432/d47tg9visn1fbf?ssl=true",
+    "sessionSecret": "7384uhejrfsd7yu230jkfnndfjkvbkwle0238ubfjbq8rgqyhbefjdksbfuiewfbj",
+    "auth0": {
+        "domain" : "jesspoemape.auth0.com",
+        "clientID": "Uk3iqCJdAfg4LGLX3USC71vSzvsXCu8Y",
+        "clientSecret": "NYbxysplhX_3xc6N9oHAOCalnoND4juixMZ7hlVzWG613xEJUXWvn26VeuauTyRv",
+        "callbackUrl": "http://localhost:3000/auth/callback"
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,65 @@
-const express = require('express');
-const massive = require('massive');
-const bodyParser = require('body-parser');
-const sessions = require('express-sessions');
-const config = require('./../config.json');
-const sc = require('./../controllers/setController');
-const cors = require('cors');
+const express = require('express'),
+     session = require('express-session'),
+     bodyParser = require('body-parser'),
+     massive = require('massive'),
+     passport = require('passport'),
+     Auth0Strategy = require('passport-auth0'),
+     config = require('./../config'),
+     sc = require('./../controllers/setController'),
+     cors = require('cors');
 
 const app = module.exports = express();
-app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:3000'}));
 
-const connectionString = config.connectionString;
-massive(connectionString).then(dbInstance => app.set('db', dbInstance)).catch(console.error);
+massive(config.connectionString).then(dbInstance => app.set('db', dbInstance)).catch(console.error);
+
+app.use(bodyParser.json());
+app.use(session({
+  resave: true, 
+  saveUninitialized: true, 
+  secret: config.sessionSecret
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new Auth0Strategy({
+    domain: config.auth0.domain,
+    clientID: config.auth0.clientID,
+    clientSecret: config.auth0.clientSecret,
+    callbackURL: config.auth0.callbackUrl
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        console.log(profile);
+        // make database calls here to check for user
+        done(null, {id: 1, username: 'Jae', email: 'j@j.com'})
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 // =========== ENDPOINTS ===========
 // app.get('/api/sets/:id', sc.getSets);
 // app.post('/api/sets', sc.addSets);
 
+// for auth
+app.get('/auth', passport.authenticate('auth0')); // authenticate the user
+app.get('/auth/callback', passport.authenticate('auth0', {successRedirect: 'http://localhost:3000/activity'})); //callback and redirect user
+app.get('/auth/me', (req, res) => { // check if someone is logged in 
+    if (!req.user) return res.status(200).send('no user');
+    res.status(200).send(req.user);
+})
+app.get('/auth/logout', (req, res) => { // log the user out and destroy the session
+    req.logout();
+    res.redirect('/');
+});
+
+// for UI
 app.post('/api/add-set', sc.addSet);
 app.get('/api/get-set-info/:id', sc.getSetInfo);
 app.get('/api/get-cards/:id', sc.getCards);
