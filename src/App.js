091a31a61d70a8e36f0037bc1ebd66805c7f202e
Jess Poemape <jesspoemape@gmail.com>
2017-07-19 13:24:48 -0600

    changed browser title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-/*import React, { Component } from 'react';
-import router from './router';
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        {router}
-      </div>
-    
-    );
-  }
-}
-
-export default App;*/
 import React from 'react';
 import router from './router';
 
