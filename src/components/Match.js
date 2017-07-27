7eff0bb55212d9eabb4401e0adcc049ebbedd1fb
Jess Poemape <jesspoemape@gmail.com>
2017-07-27 16:58:15 -0600

    match end has styling

--- src/components/Match.js
+++ src/components/Match.js
@@ -92,7 +92,8 @@ handleCardTouch(i) {
     //check if all the cards have visibility of hidden
     // if so, stop the timer and congratulate the player
     if (this.finishArr.length === this.props.cards.length * 2) {
-        console.log('here');
+        document.getElementById('game-over').style.display = 'flex';
+        document.getElementById('match-cards-container').style.display = 'none';
     }
 }
 
@@ -111,16 +112,21 @@ handleCardTouch(i) {
                     <Link to={`/set-detail/${this.props.match.params.id}`}><svg className='flash-header-icon' xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 16 16"><g><polygon points="8,4.646 8.677,5.323 6,8 13,8 13,9 6,9 8.677,11.677 8,12.354 4.146,8.5  "/></g></svg></Link>
                     <svg className='flash-header-icon' xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100"><g transform="translate(0,-952.36218)"><path d="m 89.00002,1026.3622 c 0,1.1046 -0.89543,2 -2,2 l -28.31254,0 c -0.8727,2.8728 -3.5439,5 -6.6875,5 -3.1435,0 -5.8147,-2.1272 -6.6875,-5 l -32.3125,0 c -1.1046,0 -2,-0.8954 -2,-2 0,-1.1046 0.8954,-2 2,-2 l 32.3125,0 c 0.8728,-2.8729 3.544,-5 6.6875,-5 3.1436,0 5.8148,2.1271 6.6875,5 l 28.31254,0 c 1.10457,0 2,0.8954 2,2 z m 0,-24 c 0,1.1046 -0.89543,2 -2,2 l -48.31254,0 c -0.8727,2.8728 -3.544,5 -6.6875,5 -3.1435,0 -5.8147,-2.1272 -6.6875,-5 l -12.3125,0 c -1.1046,0 -2,-0.8954 -2,-2 0,-1.1046 0.8954,-2 2,-2 l 12.3125,0 c 0.8728,-2.87288 3.544,-5.00002 6.6875,-5.00002 3.1435,0 5.8148,2.12714 6.6875,5.00002 l 48.31254,0 c 1.10457,0 2,0.8954 2,2 z m 0,-24.00002 c 0,1.1046 -0.89543,2 -2,2 l -8.3125,0 c -0.87275,2.87286 -3.54399,5 -6.6875,5 -3.14354,0 -5.81474,-2.12714 -6.68754,-5 l -52.3125,0 c -1.1046,0 -2,-0.8954 -2,-2 0,-1.1046 0.8954,-2 2,-2 l 52.3125,0 c 0.8728,-2.87286 3.544,-5 6.68754,-5 3.14351,0 5.81475,2.12714 6.6875,5 l 8.3125,0 c 1.10457,0 2,0.8954 2,2 z m -14,0 c 0,-1.68054 -1.31946,-3 -3,-3 -1.68054,0 -3.00004,1.31946 -3.00004,3 0,1.68054 1.3195,3 3.00004,3 1.68054,0 3,-1.31946 3,-3 z m -20.00004,48.00002 c 0,-1.6806 -1.3194,-3 -3,-3 -1.6805,0 -3,1.3194 -3,3 0,1.6805 1.3195,3 3,3 1.6806,0 3,-1.3195 3,-3 z m -20,-24 c 0,-1.6806 -1.3194,-3.00002 -3,-3.00002 -1.6805,0 -3,1.31942 -3,3.00002 0,1.6805 1.3195,3 3,3 1.6806,0 3,-1.3195 3,-3 z" /></g></svg>
                 </div>
-            <div className='match-timer-container'>
-                <h6 className='match-timer-title'>TIME</h6>
-                <h2 className='match-timer-time'>{this.state.time}</h2>
-            </div>
-            <div className='match-main-container'>
-                <button className='create-set-button' id='match-start-btn' onClick={() => this.handleStart()}>Start</button>
-                <div id='match-cards-container' className='match-cards-container'>
-                    {matchCards}
+                <div className='match-timer-container'>
+                    <h6 className='match-timer-title'>TIME</h6>
+                    <h2 className='match-timer-time'>{this.state.time}</h2>
                 </div>
-            </div >
+                <div className='match-main-container'>
+                    <button className='create-set-button' id='match-start-btn' onClick={() => this.handleStart()}>Start</button>
+                    <div id='match-cards-container' className='match-cards-container'>
+                        {matchCards}
+                    </div>
+                </div>
+                <div id='game-over' className='match-end-wrap'>
+                    <h2 className='flash-back-main'>Nice work!</h2>
+                    <p className='flash-back-sub'>You just studied {this.props.cards.length} terms!</p>
+                    <button className='create-set-button' >Start Over</button>
+                </div>
             </div>
         );
     }
