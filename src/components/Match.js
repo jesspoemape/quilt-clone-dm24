24660e4fbc84da31afd8ddc7fbe67d370a38bbb8
Jess Poemape <jesspoemape@gmail.com>
2017-07-27 14:03:53 -0600

    terms and defs are shuffled and displayed in match component

--- src/components/Match.js
+++ src/components/Match.js
@@ -7,10 +7,9 @@ class Match extends Component {
     constructor() {
         super();
         this.state = {
-            time: 0,
-            count: 0
+            time: 0
         }
-
+this.shuffle = this.shuffle.bind(this);
     }
 
 componentDidMount() {
@@ -19,22 +18,26 @@ componentDidMount() {
     axios.get(cardUrl).then(response => this.props.getCards(response.data)).catch(console.error, 'Error');
 }
 
+shuffle(arr) {
+    let m = arr.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = arr[m];
+        arr[m] = arr[i];
+        arr[i] = t;
+    }
+    return arr
+}
 
     render() {
-       const matchCards =  this.props.cards.map( (card, i) =>{
-           return <div>
-           <div className='match-card'>
-                    <p className='match-card-term'>{card.term}</p>
-                </div>
-                <div className='match-card'>
-                    <p className='match-card-term'>{card.definition}</p>
-                </div>
-           </div>
-           
+        const termsArr = this.props.cards.map(card => card.term);
+        const defsArr = this.props.cards.map(card => card.definition);
+        const cardsArr = termsArr.concat(defsArr);
+        const shuffledArr = this.shuffle(cardsArr);
+      const matchTerms =  shuffledArr.map( (card, i) => {
+           return <div key={i} className='match-card'><p className='match-card-term'>{card}</p></div>
        } ) 
 
-
-        const {count} = this.state;
         return (
             <div>
             <div className='match-timer-container'>
@@ -42,7 +45,8 @@ componentDidMount() {
                 <h2 className='match-timer-time'>{this.state.time}</h2>
             </div>
             <div className='match-cards-container'>
-                {matchCards}
+                {matchTerms}
+                {/*{matchDefs}*/}
             </div>
             </div>
         );
